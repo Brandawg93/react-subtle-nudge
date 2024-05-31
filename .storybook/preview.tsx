ee5baff91e29bb8a73195cd8506d9ed685d2fb25
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-05-31 14:33:26 -0400

    analytics only on prod

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,7 +3,7 @@ import type { Preview } from '@storybook/react'
 import { Title, Subtitle, Description, Primary, Controls } from '@storybook/blocks'
 import ReactGA from 'react-ga4'
 
-ReactGA.initialize('G-7P3LB8SWYT')
+if (process.env.NODE_ENV !== 'development') ReactGA.initialize('G-7P3LB8SWYT')
 
 const preview: Preview = {
   tags: ['autodocs'],
