6e2e323cfa09c16321b8ca962501b5a11ebb58d0
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-05-30 22:16:41 -0400

    sign story for swing

--- src/stories/swing/swing.stories.tsx
+++ src/stories/swing/swing.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import css from '../common.module.css'
+import signCss from './sign.module.css'
 
 import Swing from '../../components/swing'
 
@@ -19,6 +20,13 @@ export default meta
 type Story = StoryObj<typeof Swing>
 
 export const Button: Story = {
+  args: {
+    duration: '1s',
+    iterations: 0,
+    iterationDelay: '1s',
+    reverse: false,
+  },
+
   render: (args) => (
     <div className={css.wrapper}>
       <Swing {...args}>
@@ -33,6 +41,22 @@ export const Input: Story = {
     <div className={css.wrapper}>
       <Swing {...args}>
         <input type='text' />
+      </Swing>
+    </div>
+  ),
+}
+
+export const Sign: Story = {
+  render: (args) => (
+    <div className={css.wrapper}>
+      <Swing {...args}>
+        <div className={signCss.arrowWrapper}>
+          <div className={signCss.arrowUp}></div>
+          <div className={signCss.arrowUpInner}></div>
+        </div>
+        <div className={signCss.box}>
+          <span>Test Sign!</span>
+        </div>
       </Swing>
     </div>
   ),
